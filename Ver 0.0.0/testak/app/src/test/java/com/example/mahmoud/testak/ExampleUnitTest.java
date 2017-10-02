8dec06997775035e23dc81d731117a81b1d1d14f
ehsan-is-everything <ehsan.e.71@gmail.com>
2017-10-02 19:33:50 +0330

    work on version 0.1.0 startgit add .

--- Ver 0.0.0/testak/app/src/test/java/com/example/mahmoud/testak/ExampleUnitTest.java
+++ Ver 0.0.0/testak/app/src/test/java/com/example/mahmoud/testak/ExampleUnitTest.java
@@ -1,15 +1,27 @@
 package com.example.mahmoud.testak;
 
+import android.content.Context;
+import android.os.Bundle;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExampleUnitTest {
+    @Mock
+    Context mMockContext;
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void dummyMain() throws Exception {
+
+        MainActivity ma = new MainActivity();
+        ma.onCreate(Bundle.EMPTY);
+        
     }
 }
