221c6c810e8ad3f4871c16b0603176f73f37647c
ehsan-is-everything <ehsan.e.71@gmail.com>
2017-11-03 12:35:46 +0330

    test for exploitabilty addedgit add .

--- Ver 1.0.0/Testak-TextInput-button-sql-vul/app/src/test/java/com/example/lab/testak_textinput/ExampleUnitTest.java
+++ Ver 1.0.0/Testak-TextInput-button-sql-vul/app/src/test/java/com/example/lab/testak_textinput/ExampleUnitTest.java
@@ -1,6 +1,15 @@
 package com.example.lab.testak_textinput;
 
+import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.Logger;
 
 import static org.junit.Assert.*;
 
@@ -9,9 +18,19 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RunWith(MyRobolectricRunner.class)
+@Config(constants = BuildConfig.class)
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void SqliInjection_Exploitability() throws Exception {
+        Activity ma = Robolectric.setupActivity(MainActivity.class);
+        Button b= (Button) ma.findViewById(R.id.button);
+        EditText et = (EditText) ma.findViewById(R.id.editText);
+        TextView tv = (TextView) ma.findViewById(R.id.textview);
+        et.setText("a' or '1'='1");
+        b.performClick();
+
+        Logger.error((String) tv.getText(),null);
+
     }
 }
---
+++ Ver 1.0.0/Testak-TextInput-button-sql-vul/app/src/test/java/com/example/lab/testak_textinput/MyRobolectricRunner.java
@@ -0,0 +1,45 @@
+package com.example.lab.testak_textinput;
+
+import org.junit.runners.model.InitializationError;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.manifest.AndroidManifest;
+import org.robolectric.res.FileFsFile;
+import org.robolectric.res.FsFile;
+
+/**
+ * Created by ehsan on 10/6/17.
+ */
+
+public class MyRobolectricRunner extends RobolectricTestRunner {
+
+    /**
+     * Creates a runner to run {@code testClass}. Looks in your working directory for your AndroidManifest.xml file
+     * and res directory by default. Use the {@link Config} annotation to configure.
+     *
+     * @param testClass the test class to be run
+     * @throws InitializationError if junit says so
+     */
+    public MyRobolectricRunner(Class<?> testClass) throws InitializationError {
+        super(testClass);
+    }
+    protected AndroidManifest getAppManifest(Config config) {
+        AndroidManifest appManifest = super.getAppManifest(config);
+        FsFile androidManifestFile = appManifest.getAndroidManifestFile();
+
+        if (androidManifestFile.exists()) {
+            return appManifest;
+        } else {
+            String moduleRoot = getModuleRootPath(config);
+            androidManifestFile = FileFsFile.from(moduleRoot, appManifest.getAndroidManifestFile().getPath().replace("bundles", "manifests/full"));
+            FsFile resDirectory = FileFsFile.from(moduleRoot, appManifest.getResDirectory().getPath().replace("/res", "").replace("bundles", "res"));
+            FsFile assetsDirectory = FileFsFile.from(moduleRoot, appManifest.getAssetsDirectory().getPath().replace("/assets", "").replace("bundles", "assets"));
+            return new AndroidManifest(androidManifestFile, resDirectory, assetsDirectory);
+        }
+    }
+
+    private String getModuleRootPath(Config config) {
+        String moduleRoot = config.constants().getResource("").toString().replace("file:", "");
+        return moduleRoot.substring(0, moduleRoot.indexOf("/build"));
+    }
+}
