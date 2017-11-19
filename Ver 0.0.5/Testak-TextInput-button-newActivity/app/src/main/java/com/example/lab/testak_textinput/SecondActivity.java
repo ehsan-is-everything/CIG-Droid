7a60e38f52cf8439b71075c52e0a93aef049d8d9
ehsan-is-everything <ehsan.e.71@gmail.com>
2017-11-19 18:19:08 +0330

    fdroid tests

--- Ver 0.0.5/Testak-TextInput-button-newActivity/app/src/main/java/com/example/lab/testak_textinput/SecondActivity.java
+++ Ver 0.0.5/Testak-TextInput-button-newActivity/app/src/main/java/com/example/lab/testak_textinput/SecondActivity.java
@@ -6,14 +6,13 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         int id = getIntent().getIntExtra("ID",0);
         if(id==125) {
-                Log.e("TEST","ERROR FOUND!!!");
+            Log.e("TEST","ERROR FOUND!!!");
             assert (false);
         }
     }
